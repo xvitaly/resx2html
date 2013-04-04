5aebb01493714096781c3bad4e966bab23a480f3
V1TSK <vitaly@easycoding.org>
2013-04-04 23:39:03 +0600

    Uploaded from SVN repository.

---
+++ resx2html/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("resx2html")]
+[assembly: AssemblyDescription("resx2html")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EasyCoding Team")]
+[assembly: AssemblyProduct("resx2html")]
+[assembly: AssemblyCopyright("(c) 2005-2011 EasyCoding Team. All rights reserved.")]
+[assembly: AssemblyTrademark("EasyCoding, EasyCoding Software Group and EasyCoding Team are trademarks of EasyCoding Team.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2fc8ec32-bf7b-4526-93e8-0b6ea08c9c52")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.16")]
+[assembly: AssemblyFileVersion("0.1.0.16")]
+[assembly: NeutralResourcesLanguageAttribute("")]
