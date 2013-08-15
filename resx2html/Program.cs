75d689d22fa44c76e08fe531dcd4f4f50f5a4ab6
V1TSK <vitaly@easycoding.org>
2013-08-15 23:46:55 +0600

    * Code refactored.

--- resx2html/Program.cs
+++ resx2html/Program.cs
@@ -8,9 +8,14 @@ namespace resx2html
 {
     class Program
     {
-        static void Main(string[] Args)
+        static void ConfigureConsole(string Title, ConsoleColor Color)
         {
-            Console.Title = Properties.Resources.AppName;
+            Console.Title = Title;
+            Console.ForegroundColor = Color;
+        }
+
+        static void ShowSplash()
+        {
             Console.WriteLine("###########################################################################");
             Console.WriteLine("#              WELCOME TO RESX2HTML (.NET RESOURCE CONVERTER)             #");
             Console.WriteLine("#           This console program will convert resx file to HTML.          #");
@@ -22,48 +27,57 @@ namespace resx2html
             Console.WriteLine("#                       Part of SRC Repair project.                       #");
             Console.WriteLine("###########################################################################");
             Console.WriteLine();
+        }
+
+        static void DoConversion(string Source, string Dest)
+        {
+            try
+            {
+                using (StreamWriter CFile = new StreamWriter(Dest, false, Encoding.UTF8))
+                {
+                    CFile.WriteLine(Properties.Resources.HTMLHeader);
+                    XmlDocument XMLD = new XmlDocument();
+                    using (FileStream XMLFS = new FileStream(Source, FileMode.Open, FileAccess.Read))
+                    {
+                        XMLD.Load(XMLFS);
+                        XmlNodeList XMLNList = XMLD.GetElementsByTagName("data");
+                        for (int i = 0; i < XMLNList.Count; i++)
+                        {
+                            try
+                            {
+                                XmlElement ElID = (XmlElement)XMLD.GetElementsByTagName("data")[i];
+                                if (String.IsNullOrWhiteSpace(ElID.GetAttribute("type")))
+                                {
+                                    CFile.WriteLine(String.Format(Properties.Resources.HTMLRow, ElID.GetAttribute("name"), ElID.GetElementsByTagName("value")[0].InnerText));
+                                }
+                            }
+                            catch (Exception Ex) { CFile.WriteLine(String.Format(Properties.Resources.HTMLCommFormat, Ex.Message)); }
+                        }
+                        XMLFS.Close();
+                    }
+                    CFile.WriteLine(Properties.Resources.HTMLFooter);
+                    CFile.Close();
+                }
+            }
+            catch (Exception Ex) { Console.WriteLine(String.Format(Properties.Resources.ExcptMsg, Ex.Message)); }
+        }
+        
+        static void Main(string[] Args)
+        {
+            ConfigureConsole(Properties.Resources.AppName, ConsoleColor.Green);
+            ShowSplash();
+
             if (Args.Count() > 1)
             {
                 if (File.Exists(Args[0]))
                 {
                     Console.WriteLine(String.Format("Source file: {0}\nDestination file: {1}\n", Args[0], Args[1]));
                     Console.Write("Starting conversion...");
-                    try
-                    {
-                        using (StreamWriter CFile = new StreamWriter(Args[1], false, Encoding.UTF8))
-                        {
-                            CFile.WriteLine(Properties.Resources.HTMLHeader);
-                            XmlDocument XMLD = new XmlDocument();
-                            using (FileStream XMLFS = new FileStream(Args[0], FileMode.Open, FileAccess.Read))
-                            {
-                                XMLD.Load(XMLFS);
-                                XmlNodeList XMLNList = XMLD.GetElementsByTagName("data");
-                                for (int i = 0; i < XMLNList.Count; i++)
-                                {
-                                    try
-                                    {
-                                        XmlElement ElID = (XmlElement)XMLD.GetElementsByTagName("data")[i];
-                                        if (String.IsNullOrWhiteSpace(ElID.GetAttribute("type")))
-                                        {
-                                            CFile.WriteLine(String.Format(Properties.Resources.HTMLRow, ElID.GetAttribute("name"), ElID.GetElementsByTagName("value")[0].InnerText));
-                                        }
-                                    }
-                                    catch (Exception Ex) { CFile.WriteLine(String.Format("<!-- {0} -->", Ex.Message)); }
-                                }
-                                XMLFS.Close();
-                            }
-                            CFile.WriteLine(Properties.Resources.HTMLFooter);
-                            CFile.Close();
-                            Console.WriteLine(" Done.\n\nGoodbye.");
-                        }
-                    }
-                    catch (Exception Ex) { Console.WriteLine(String.Format(Properties.Resources.ExcptMsg, Ex.Message)); }
+                    DoConversion(Args[0], Args[1]);
+                    Console.WriteLine(" Done.\n\nGoodbye.");
                 }
             }
-            else
-            {
-                Console.WriteLine(Properties.Resources.WlxMsg);
-            }
+            else { Console.WriteLine(Properties.Resources.WlxMsg); }
             #if DEBUG
             Console.ReadKey();
             #endif
--- resx2html/Properties/Resources.Designer.cs
+++ resx2html/Properties/Resources.Designer.cs
@@ -79,6 +79,15 @@ namespace resx2html.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to &lt;!-- {0} --&gt;.
+        /// </summary>
+        internal static string HTMLCommFormat {
+            get {
+                return ResourceManager.GetString("HTMLCommFormat", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to &lt;/table&gt;&lt;/div&gt;&lt;div align=\&quot;right\&quot;&gt;(c) EasyCoding Team&lt;/div&gt;&lt;/body&gt;.
         /// </summary>
         internal static string HTMLFooter {
